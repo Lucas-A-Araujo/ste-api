dbc1fe40ee6e87b4ffa18d34aceb3f955562ff1d
Lucas-A-Araujo <lucas.al.araujo10@gmail.com>
2025-07-31 18:25:43 -0300

    test: add .env config to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,16 @@ import { PersonModule } from './person/person.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'docker',
-      password: 'docker',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 5432,
+      username: process.env.DATABASE_USER || 'docker',
+      password: process.env.DATABASE_PASSWORD || 'docker',
+      database: process.env.DATABASE_NAME || 'postgres',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      ssl: process.env.NODE_ENV === 'production' ? {
+        rejectUnauthorized: false
+      } : false,
     }),
     PersonModule,
   ],
