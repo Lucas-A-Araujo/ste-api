3286bbf4e96431f1bdca53014d335435a01c544b
Lucas-A-Araujo <lucas.al.araujo10@gmail.com>
2025-07-31 15:50:51 -0300

    chore: create person controller

---
+++ src/person/person.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { PersonService } from './person.service';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { UpdatePersonDto } from './dto/update-person.dto';
+
+@Controller('people')
+export class PersonController {
+  constructor(private readonly service: PersonService) {}
+
+  @Post()
+  create(@Body() dto: CreatePersonDto) {
+    return this.service.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.service.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: UpdatePersonDto) {
+    return this.service.update(+id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.remove(+id);
+  }
+}
