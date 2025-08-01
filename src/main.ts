76ea7ee8a4e8a0a326bc054f60dcd7ae892940c3
Lucas-A-Araujo <lucas.al.araujo10@gmail.com>
2025-08-01 16:34:13 -0300

    chore: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,25 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Configuração de versionamento da API
   app.enableVersioning({
     type: VersioningType.URI,
   });
+  
+  const config = new DocumentBuilder()
+    .setTitle('Ste CRUD API')
+    .setDescription('API para gerenciamento de pessoas - Ste')
+    .setVersion('1.0')
+    .addTag('people', 'Operações relacionadas a pessoas')
+    .addBearerAuth()
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT ?? 4001);
