51fefe6b6c36c9a188eabac34aac7e02f2a870f7
Lucas-A-Araujo <lucas.al.araujo10@gmail.com>
2025-08-01 11:19:18 -0300

    feat: add version control to all routes in person

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -2,13 +2,22 @@ import { Body, Controller, Delete, Get, Param, Post, Put, Version } from '@nestj
 import { PersonService } from './person.service';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
+import { CreatePersonV2Dto } from './dto/create-person-v2.dto';
+import { UpdatePersonV2Dto } from './dto/update-person-v2.dto';
 
 @Controller('people')
 export class PersonController {
   constructor(private readonly service: PersonService) {}
 
   @Post()
+  @Version('1')
   create(@Body() dto: CreatePersonDto) {
+    return this.service.create(dto);
+  }
+
+  @Post()
+  @Version('2')
+  createV2(@Body() dto: CreatePersonV2Dto) {
     return this.service.create(dto);
   }
 
@@ -25,17 +34,38 @@ export class PersonController {
   }
 
   @Get(':id')
+  @Version('1')
   findOne(@Param('id') id: string) {
     return this.service.findOne(+id);
   }
 
+  @Get(':id')
+  @Version('2')
+  findOneV2(@Param('id') id: string) {
+    return this.service.findOne(+id);
+  }
+
   @Put(':id')
+  @Version('1')
   update(@Param('id') id: string, @Body() dto: UpdatePersonDto) {
     return this.service.update(+id, dto);
   }
 
+  @Put(':id')
+  @Version('2')
+  updateV2(@Param('id') id: string, @Body() dto: UpdatePersonV2Dto) {
+    return this.service.update(+id, dto);
+  }
+
   @Delete(':id')
+  @Version('1')
   remove(@Param('id') id: string) {
+    return this.service.remove(+id);
+  }
+
+  @Delete(':id')
+  @Version('2')
+  removeV2(@Param('id') id: string) {
     return this.service.remove(+id);
   }
 }
